4c79a1a09c81c3031845cd790fd327ee71904a75
bmalenovska <165932281+bmalenovska@users.noreply.github.com>
2024-04-19 16:22:59 +0300

    Update and rename arrays.java to loops.java

---
+++ loops.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Packing a backpack for a trip
+        for (int i = 0; i < 10; i++) {
+            int itemNumber = i + 1; // Adjust item number for human-friendly output
+            System.out.println("Packing item number " + itemNumber);
+            // Pack item number itemNumber into your backpack
+        }
+        System.out.println("Backpack is packed and ready to go!");
+    }
+}
